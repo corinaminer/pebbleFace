266237556e4466cf5b0d4ef881bec0ea34d63923
Corina Miner <corinaminer@Corinas-MacBook-Pro.local>
2017-09-18 21:07:20 -0700

    First basic watch face as per tutorial

---
+++ build/basalt/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "pebbleFace",
+  .company = "Corina Miner",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE | PROCESS_INFO_PLATFORM_BASALT,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x7C, 0xD0, 0x52, 0x67, 0x7C, 0x97, 0x44, 0x84, 0xBE, 0x93, 0x84, 0x5B, 0xD9, 0xDE, 0x25, 0x18 },
+  .virtual_size = 0xb6b6
+};
